c3a29e33b043189cf34daafcc640dd7d0a0f576c
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-09-17 17:32:49 -0600

    finished mvp for all three assignments

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
   // GIVEN THIS PROBLEM:
 
@@ -36,29 +36,86 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
+
 */
 
 
+
+//question 1
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr);
+}
+const length = (arr) =>{
+  return arr.length;
 }
 
+console.log(getLength(items, length));
+//end of question 1
+
+
+
+
+//question 2
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr);
+};
+const lastItem = (arr) =>{
+  return arr[arr.length-1];
 }
+console.log(last(items, lastItem));
+//end of question 2
 
+
+
+
+//question 3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y)
+}
+const add = (x, y) => {
+  return x + y;
 }
 
+console.log(sumNums(2,3,add));
+
+//end of question 3
+
+
+
+
+//question 4
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
+};
+
+const multiply = (x,y) =>{
+  return x * y;
 }
 
+console.log(multiplyNums(4,5,multiply))
+//end of question 4
+
+//question 5
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(item, list);
+};
+
+const check = (item, list) => {
+  for (let i =0; i<list.length; i++){
+    if (list[i] === item) {
+      return true;
+    };
+  }
+  return false;
 }
+console.log(contains("Gum", items, check))
+//end of question 5
 
 /* STRETCH PROBLEM */
 
